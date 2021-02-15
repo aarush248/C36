a0cebcc38daddd11b533e403aba9fa5a796b580d
aarush248 <71240722+aarush248@users.noreply.github.com>
2021-02-14 20:08:16 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var playerCount=0, gameState=0;
+var form, game, player;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game()
+  game.getState()
+  game.start()
+  
+  
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
+
